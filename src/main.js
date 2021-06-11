9c94a57120e2049048d9cce6c42366e45ff78868
JaniceHe264 <xianzhanyou64116@yeah.net>
2021-06-12 06:49:34 +0800

    整合阿里图标库

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 //======引入ElementUiPlus结束========
 
+//======引入阿里图标库开始======
+import '@/assets/icon/iconfont.css'
+//======引入阿里图标库结束======
+
 createApp(App)
   .use(store)
   .use(router)
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,6 +3,7 @@
     <h1>This is an about page</h1>
     <div class="box">
       <p>测试文字</p>
+      <span class="iconfont icon-dianzan"></span>
     </div>
   </div>
 </template>
